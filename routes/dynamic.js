866e529c9965cd2f04559c5c8933184a6cdc7276
Your Name <you@example.com>
2025-02-11 13:30:01 +0200

    the second lab work was completed successfully

---
+++ routes/dynamic.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+
+const students = [
+  { name: 'Ситник Дмитро Сергійович', city: 'Кропивницький', photo: 'frog.png',  about: 'Мене звати Дмитро, я люблю котиків, вчуся в КПІ'},
+  { name: 'Погорілець Владислав Миколайович', city: 'Кропивницький', photo: 'frog.png',  about: 'Мене звати Влад, я люблю котиків, вчуся в КПІ'},
+  { name: 'Романюк Діана Петрівна', city: 'Кропивницький', photo: 'frog.png',  about: 'Мене звати Діана, я люблю котиків, вчуся в КПІ'},
+  { name: 'Майко Денис Ростиславович', city: 'Кропивницький', photo: 'frog.png',  about: 'Мене звати Денис, я люблю котиків, вчуся в КПІ'},
+  { name: 'Мосієвич Богдана Юріївна', city: 'Кропивницький', photo: 'frog.png',  about: 'Мене звати Богдана, я люблю котиків, вчуся в КПІ'}
+];
+
+router.get('/', function(req, res, next) {
+  res.render('dynamic', { title: 'Dynamic Page', students: students});
+});
+
+module.exports = router;
